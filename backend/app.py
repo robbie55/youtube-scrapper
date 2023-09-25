7f1077741c128069476508670dc46adcb287f919
Robbie McLaughlin <rmclaughlin@Robbie-McLaughlins-Mac-J47RD2L4T5.local>
2023-09-25 10:18:24 -0500

    pushing changed backend and frontend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify, request
-from scrape import scrapYoutube
+import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
   if (request.method == "GET"):
-    data = "hello world"
+    data = requests.get('https://www.googleapis.com/youtube/v3/search')
     return jsonify({'data': data})
   
 if __name__ == '__main__':
   app.run(debug=True, port=5000)
+
+
+# https://www.youtube.com/watch?v=4RIkc330cZc
--- backend/scrape.py
+++ backend/scrape.py
@@ -1,19 +1,19 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get('https://www.youtube.com/watch?v=6G3L94NXGc4')
-time.sleep(3)
+# driver = webdriver.Chrome()
+# driver.get('https://www.youtube.com/watch?v=6G3L94NXGc4')
+# time.sleep(3)
 
-actionsElem = driver.find_element(By.XPATH, '//*[@id="button-shape"]/button/yt-touch-feedback-shape/div/div[2]')
-actionsElem.click()
-time.sleep(3)
-transcriptElem = driver.find_element(By.XPATH, '//*[@id="items"]/ytd-menu-service-item-renderer[4]')
-transcriptElem.click()
-time.sleep(3)
+# actionsElem = driver.find_element(By.XPATH, '//*[@id="button-shape"]/button/yt-touch-feedback-shape/div/div[2]')
+# actionsElem.click()
+# time.sleep(3)
+# transcriptElem = driver.find_element(By.XPATH, '//*[@id="items"]/ytd-menu-service-item-renderer[4]')
+# transcriptElem.click()
+# time.sleep(3)
 
 
 
-driver.close()
+# driver.close()
